07d393ec283544a8e715069db28b2f2171901c62
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-12 20:37:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@
 def nyc_pigeon_organizer(data)
  nh = {} 
  data.each do |att| 
-   att.each do |cat| 
-     cat.each do |pig| 
+   data[att].each do |cat| 
+     data[att][cat].each do |pig| 
        nh[pig][att].push(cat) 
      end 
    end 
